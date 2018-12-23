bd2c93df6f46bcf4a43463583ec98448907236ad
sweezy793 <sarthaks793@gmail.com>
2018-12-23 23:58:23 +0530

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,49 @@
 const express=require('express')
+const gravatar=require('gravatar')
+const bcrypt=require('bcryptjs')
+
 const router=express.Router();
+
+
+const User=require('../../models/User')
 
 router.get('/test',(req,res)=>{
     res.json({msg:"User works"})
 });
+
+router.post('/register',(req,res)=>{
+    User.findOne({email:req.body.email})
+    .then(user=>{
+        if(user)
+        {
+            return res.status(400).json({email:'Email already exists'}); // sending error status code
+        }
+        else{
+            const avatar=gravatar.url(req.body.email,{
+                s:'200', //Size
+                r:'pg', //rating
+                d:'mm' //Default
+            })
+
+            const newUser=new User({
+                name:req.body.name,
+                email:req.body.email,
+                password:req.body.password,
+                avatar
+            })
+
+            bcrypt.genSalt(10,(err,salt)=>{
+                bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                    if(err)
+                    throw err;
+                    newUser.password=hash; //setting password to the hash
+                    newUser.save()
+                        .then(user=>res.json(user))
+                        .catch(err=>console.log(err));
+                })
+            })
+        }
+    })
+})
 
 module.exports=router;
