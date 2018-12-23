67be2528c5d599f594054d93d20a245efa24ae58
sweezy793 <sarthaks793@gmail.com>
2018-12-24 00:09:42 +0530

    User partial login

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,6 +11,8 @@ router.get('/test',(req,res)=>{
     res.json({msg:"User works"})
 });
 
+/////REGISTER
+
 router.post('/register',(req,res)=>{
     User.findOne({email:req.body.email})
     .then(user=>{
@@ -45,5 +47,30 @@ router.post('/register',(req,res)=>{
         }
     })
 })
+
+
+/////LOGIN
+router.post('/login',(req,res)=>{
+    const email=req.body.email;
+    const password=req.body.password;
+
+    User.findOne({email})
+        .then(user=>{
+            if(!user){
+                return res.status(404).json({email:'User not found'});
+            }
+
+            bcrypt.compare(password,user.password)   //password is the password typed at login and user.password is the hashed password stored in db
+                .then(isMatch=>{
+                    if(isMatch){
+                        res.json({msg:'Success'})
+                    }
+                    else{
+                        return res.status(400).json({password:'Incorrect password'})
+                    }
+                })
+        })
+})
+
 
 module.exports=router;
