ffe3c54c1e926857858835f9574f33f7490c4e25
sweezy793 <sarthaks793@gmail.com>
2018-12-26 23:16:31 +0530

    Delete route and Post model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -212,7 +212,72 @@ router.post(
       });
     }
   );
-  
 
+
+
+
+    //////////Delete PROFILE EXPERIENCE
+
+    router.delete(
+      '/experience/:exp_id',
+      passport.authenticate('jwt', { session: false }),
+      (req, res) => {
+    
+        Profile.findOne({ user: req.user.id }).then(profile => {
+          const removeIndex=profile.experience
+            .map(item=>item.id)
+            .indexOf(req.params.exp_id);
+
+            //Splicing it out of array
+            profile.experience.splice(removeIndex,1);
+
+            //Save
+            profile
+              .save()
+              .then(profile=>res.json(profile));
+        }).catch(err=>res.status(404).json(err)); 
+      }
+    );
+
+
+
+ //////////Delete PROFILE EDUCATION
+
+ router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const removeIndex=profile.education
+        .map(item=>item.id)
+        .indexOf(req.params.edu_id);
+
+        //Splicing it out of array
+        profile.education.splice(removeIndex,1);
+
+        //Save
+        profile
+          .save()
+          .then(profile=>res.json(profile));
+    }).catch(err=>res.status(404).json(err)); 
+  }
+);
+
+
+  
+ //////////Delete PROFILE and USER
+
+ router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({user:req.user.id})
+      .then(()=>{
+        User.findOneAndRemove({_id:req.user.id})
+          .then(()=>res.json({success:true}))
+      })
+  }
+);
 
 module.exports=router;
