bbc18d3ac919391b9c3ad77ea8bb82c1dc680e68
sweezy793 <sarthaks793@gmail.com>
2019-01-06 22:37:46 +0530

    All profiles view

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
+import Profiles from './components/profiles/Profiles';
 
 import PrivateRoute from './components/common/PrivateRoute';
 
@@ -54,6 +55,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
