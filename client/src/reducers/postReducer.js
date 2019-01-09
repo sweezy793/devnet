e2b534ff046b5b26e8d6d8e59f78d33735923c4c
sweezy793 <sarthaks793@gmail.com>
2019-01-10 01:29:50 +0530

    Post feed page changes

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST } from '../actions/types';
+import { ADD_POST, POST_LOADING, GET_POST, GET_POSTS } from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -8,11 +8,29 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case GET_POST:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
     case ADD_POST:
       return {
         ...state,
         posts: [action.payload, ...state.posts]
       };
+
     default:
       return state;
   }
