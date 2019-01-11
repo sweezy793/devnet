99b68ca0bdc13cb85a95b8e5eb51b1479b3f351c
sweezy793 <sarthaks793@gmail.com>
2019-01-11 23:46:11 +0530

    Deployment

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports={
-    mongoURI:'mongodb://sarthak:admin123@ds123844.mlab.com:23844/devnet',
-    secretOrKey:'afasfdas'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const bodyParser=require('body-parser')
-const passport=require('passport')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
-const users=require('./routes/api/users')
-const profile=require('./routes/api/profile')
-const posts=require('./routes/api/posts')
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
-const app=express();
+const app = express();
 
-const db=require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect(db,{ useNewUrlParser: true})
-    .then(()=>{
-        console.log('DB connected')
-    })
-    .catch(err=>console.log(err));
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('DB connected');
+  })
+  .catch(err => console.log(err));
 
 app.use(passport.initialize());
 
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
-app.use('/api/users',users)
-app.use('/api/profile',profile)
-app.use('/api/posts',posts)
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-const port=process.env.PORT||5000
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
